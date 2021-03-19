ce1d963ccdd592cf8f57c5d0a5a27eff870a12e2
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-03-18 21:35:23 -0300

    Registrando st-category e st-product no eureka

--- st-product/src/main/java/com/jfb/stproduct/StProductApplication.java
+++ st-product/src/main/java/com/jfb/stproduct/StProductApplication.java
@@ -2,7 +2,9 @@ package com.jfb.stproduct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class StProductApplication {
 
