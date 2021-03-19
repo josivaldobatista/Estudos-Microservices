262a3523c680a0f13b274f93485474856e66fec3
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-03-18 21:24:33 -0300

    Eureka server

---
+++ st-eureka-server/src/test/java/com/jfb/steurekaserver/StEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfb.steurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
